cd923345490db9b86f444611eb84246309ee45c9
LIAOWEI97 <lw993859222@gmail.com>
2021-02-16 18:06:14 +0800

    1.0

---
+++ src/main/java/com/itheima/DroolsApplication.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 99385
+ */
+@SpringBootApplication
+public class DroolsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DroolsApplication.class, args);
+    }
+}
